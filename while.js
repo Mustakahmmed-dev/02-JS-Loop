5b0d1546ed4c6d88a5196fa4c780831939de0fc7
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-08 23:33:17 +0600

    sum, even and odd numbers together using while loop

--- while.js
+++ while.js
@@ -2,6 +2,28 @@
 let num = 1;
 
 while(num <= 20){
-    console.log("Number: ", num);
+    // console.log("Number: ", num);
     num++;
+}
+
+let myCurrentNum = 1;
+let sum = 0;
+while(myCurrentNum <= 10){
+    sum = sum + myCurrentNum;
+    // console.log("Sum: ", sum);
+    myCurrentNum++;
+    
+}
+
+// Output even and odd numbers together
+let currentNum = 1;
+
+while(currentNum < 20){
+    if(currentNum % 2 === 0){
+        console.log("Even Num ", currentNum);
+    }
+    else{
+        console.log("Odd Num ", currentNum);
+    }
+    currentNum++;
 }
