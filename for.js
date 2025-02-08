cf9ee63547d3eb818fa5636377c3c62f08c19cf9
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-08 23:03:34 +0600

    JS for of loop practiced

---
+++ for.js
@@ -0,0 +1,6 @@
+// Output an array items of fruits using for of loop
+let fruits = ["mango", "banana", "grape", "watermelon", "guava", "orange", "apple"];
+
+for(let fruit of fruits){
+    console.log("I love this fruit :", fruit)
+}
