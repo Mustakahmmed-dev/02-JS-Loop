de0b62f1c42a838b4364fc67e26aec8009fb406e
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-09 22:16:51 +0600

    EVen numbers from 78 to 98

--- for-loop-task.js
+++ for-loop-task.js
@@ -1,12 +1,22 @@
+// Task 1
 // Display a sentence for 60 times "I will invest at least 6 hrs every single day for next 60 days!"
 
 for(let msg = 1; msg <= 60; msg++){
     // console.log(msg + ": I will invest at least 6 hrs every single day for next 60 days!");
 }
 
+// Task 2
 // Find all the odd numbers from 61 to 100
 for(let num = 61; num <= 100; num++){
     if(num % 2 === 1){
-        console.log("Odd num: ", num)
+        // console.log("Odd num: ", num)
+    }
+}
+
+// Task 3
+// Find all the even numbers from 78 to 98
+for(let evenNum = 78; evenNum <= 98; evenNum++){
+    if(evenNum % 2 === 0){
+        console.log("Even Number is: ", evenNum);
     }
 }
