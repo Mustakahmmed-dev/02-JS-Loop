f617c683098c78f44c5561d762b27e4991e3f824
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-09 22:13:44 +0600

    Printed the odd numbers from 61 to 100

---
+++ for-loop-task.js
@@ -0,0 +1,12 @@
+// Display a sentence for 60 times "I will invest at least 6 hrs every single day for next 60 days!"
+
+for(let msg = 1; msg <= 60; msg++){
+    // console.log(msg + ": I will invest at least 6 hrs every single day for next 60 days!");
+}
+
+// Find all the odd numbers from 61 to 100
+for(let num = 61; num <= 100; num++){
+    if(num % 2 === 1){
+        console.log("Odd num: ", num)
+    }
+}
--- js-loop-task.js
+++
@@ -1,5 +0,0 @@
-// Display a sentence for 60 times "I will invest at least 6 hrs every single day for next 60 days!"
-
-for(let msg = 1; msg <= 60; msg++){
-    console.log(msg + ": I will invest at least 6 hrs every single day for next 60 days!");
-}
