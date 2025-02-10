29f83b9b91a88595d5c0f7cdafa7026a38484996
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-10 10:42:46 +0600

    odd numbers with while loop

--- while-loop-task.js
+++ while-loop-task.js
@@ -2,6 +2,17 @@
 // "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
 let commitment = 1;
 while(commitment <= 60){
-    console.log(commitment, " I will invest at least 6 hrs every single day for next 60 days!")
+    // console.log(commitment, " I will invest at least 6 hrs every single day for next 60 days!")
     commitment++;
 }
+
+// Task 2
+// Find all the odd numbers from 61 to 100.
+let initOdd = 61;
+while(initOdd <= 100){
+    if(initOdd % 2 === 1){
+        console.log("The odd num: ", initOdd);
+    }
+    initOdd++;
+}
+
