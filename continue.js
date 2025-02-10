f8d59b7992e6d7220b62c93b1b37f18c51206949
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-10 12:37:55 +0600

    odd numbers and skipp numbers divisible by 5

--- continue.js
+++ continue.js
@@ -4,5 +4,13 @@ for(let i = 1; i <= 40; i++){
     if(i % 2 === 1){
         continue;
     }
-    console.log("The continue val ", i);
+    // console.log("The continue val ", i);
+}
+
+// Task 2
+// display odd number from 55 to 85 and skip the numbers divisible by 5.
+for(let y = 55; y <= 85; y++){
+    if(y % 2 === 1 && y % 5 !== 0){
+        console.log("Odd number: ", y);
+    }
 }
